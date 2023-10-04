441254ab524a4db88dd9b3a5a1d973a74403b8e1
Pirann Suhkia <pirannsukhia@gmail.com>
2023-10-05 03:47:21 +0530

    list the times when they get completed

--- src/scripts/stopwatch.js
+++ src/scripts/stopwatch.js
@@ -4,6 +4,7 @@ let timerInterval;
 let timerStartTime;
 let timerRunning = false;
 let spacebarPressed = false;
+const finalTimesList = document.getElementById("finalTimesList"); // Get the ordered list element
 
 function updateTimer() {
   const currentTime = Date.now();
@@ -21,16 +22,15 @@ function updateTimer() {
 // Event listener for spacebar press
 document.addEventListener("keydown", function (event) {
   if (event.key === " ") {
-    event.preventDefault(); // Prevent the spacebar from scrolling the page
+    event.preventDefault();
     if (timerRunning) {
-      // If the stopwatch is running, stop it and go to inactive state
       clearInterval(timerInterval);
       timerRunning = false;
-      timerDisplay.style.backgroundColor = ""; // Set to inactive state (no color)
+      timerDisplay.style.backgroundColor = "";
+      updateFinalTimes(); // Update the list when a solve is completed
     } else if (!spacebarPressed) {
-      // If the spacebar was not previously pressed, change to ready state
       spacebarPressed = true;
-      timerDisplay.style.backgroundColor = "#FFA500"; // Set to ready state (yellow)
+      timerDisplay.style.backgroundColor = "#FFA500";
     }
   }
 });
@@ -38,22 +38,21 @@ document.addEventListener("keydown", function (event) {
 // Event listener for spacebar release to start the stopwatch
 document.addEventListener("keyup", function (event) {
   if (event.key === " " && spacebarPressed && !timerRunning) {
-    event.preventDefault(); // Prevent the spacebar from scrolling the page
-    // Start the timer
+    event.preventDefault();
     timerStartTime = Date.now();
     timerInterval = setInterval(updateTimer, 10);
     timerRunning = true;
-    timerDisplay.style.backgroundColor = "#00FF00"; // Set to active state (green)
+    timerDisplay.style.backgroundColor = "#00FF00";
     spacebarPressed = false;
   }
 });
 
-// Event listener for any key press to stop the stopwatch and go to inactive state
-document.addEventListener("keydown", function (event) {
-  if (timerRunning) {
-    // Any key press stops the timer and goes to inactive state
-    clearInterval(timerInterval);
-    timerRunning = false;
-    timerDisplay.style.backgroundColor = ""; // Set to inactive state (no color)
+// Function to update the list of final times
+function updateFinalTimes() {
+  const currentTime = timerDisplay.innerHTML;
+  if (currentTime !== "Hold Spacebar to Ready\nRelease to Start") {
+    const listItem = document.createElement("li");
+    listItem.textContent = currentTime;
+    finalTimesList.appendChild(listItem);
   }
-});
+}
